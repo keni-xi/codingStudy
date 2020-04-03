7353a47b6ac5a268cb821721a5e2dccb290d4b9b
keni-xi <2352991964@qq.com>
2020-04-04 00:05:11 +0800

    mpVue网易严选小程序

---
+++ mapVue-koa/mpvue-shop-node/config.js
@@ -0,0 +1,13 @@
+const CONF = {
+  port: '5757',
+  mysql: {
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    db: 'nodemysql',
+    pass: '232425',
+    char: 'utf8mb4'
+  }
+}
+
+module.exports = CONF
