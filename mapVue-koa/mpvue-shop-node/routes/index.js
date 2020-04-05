48e42c62730a7b213747a69a89c28417b6653212
keni-xi <2352991964@qq.com>
2020-04-05 22:36:45 +0800

    goods页页面

--- mapVue-koa/mpvue-shop-node/routes/index.js
+++ mapVue-koa/mpvue-shop-node/routes/index.js
@@ -6,4 +6,13 @@ const controllers = require('../controllers/index')
 // 首页相关的接口
 router.get('/index/index', controllers.home.index)
 
+// 搜索相关的接口
+router.get('/search/indexaction', controllers.search.index.indexAction)
+router.post('/search/addhistoryaction', controllers.search.index.addHistoryAction)
+router.post('/search/clearhistoryAction', controllers.search.index.clearHistoryAction) // 清除搜索历史
+router.get('/search/helperaction', controllers.search.index.helperAction) // 搜索提示
+
+// 商品详情
+router.get('/goods/detailaction', controllers.goods.index.detailAction)
+
 module.exports = router
